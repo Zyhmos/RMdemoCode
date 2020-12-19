8482e0ecf108332286e6a74d9e02b2c539198267
Zyhmos <42747934+Zyhmos@users.noreply.github.com>
2020-12-19 20:33:43 +0200

    Almost done, polish and comments some time tomorrow

--- SOURCE/RMdemo/WebApplication1/Controllers/HomeController.cs
+++ SOURCE/RMdemo/WebApplication1/Controllers/HomeController.cs
@@ -17,6 +17,11 @@ namespace WebApplication1.Controllers
             Manager = new ProductSQL()
         };
 
+        public ActionResult Producter()
+        {
+            return View();
+        }
+
         public ActionResult Index()
         {
             return RedirectToAction("Product", "Home");
@@ -30,8 +35,24 @@ namespace WebApplication1.Controllers
 
         public ActionResult ProductNew()
         {
-            model.Types = model.Manager.GetAllTypeData();
-            return View(model);
+            if (this.Request.RequestType != "POST")
+            {
+                model.Types = model.Manager.GetAllTypeData();
+                return View(model);
+            }
+
+            var product = new Product
+            {
+                ProductID = Convert.ToInt32(Request.Form["Selected.ProductID"]),
+                Description = Request.Form["Selected.Description"],
+                Code = Request.Form["Selected.Code"],
+                TypeID = Convert.ToInt32(Request.Form["Selected.TypeID"]),
+                Amount = Convert.ToInt32(Request.Form["Selected.Amount"]),
+                Price = Convert.ToDouble(Request.Form["Selected.Price"])
+            };
+            model.Manager.AddNew(product);
+            return RedirectToAction("Product", "Home");
+
         }
 
         [HttpGet]
@@ -41,6 +62,21 @@ namespace WebApplication1.Controllers
             model.Selected = model.Manager.FindByID(id);
             model.Types = model.Manager.GetAllTypeData();
             return View(model);
+        }
+
+        public ActionResult ProductEdit()
+        {
+            var product = new Product
+            {
+                ProductID = Convert.ToInt32(Request.Form["Selected.ProductID"]),
+                Description = Request.Form["Selected.Description"],
+                Code = Request.Form["Selected.Code"],
+                TypeID = Convert.ToInt32(Request.Form["Selected.TypeID"]),
+                Amount = Convert.ToInt32(Request.Form["Selected.Amount"]),
+                Price = Convert.ToDouble(Request.Form["Selected.Price"])
+            };
+            model.Manager.Edit(product);
+            return RedirectToAction("Product", "Home");
         }
 
         [HttpGet]
@@ -55,12 +91,12 @@ namespace WebApplication1.Controllers
         {
             var product = new Product
             {
-                ProductID = Convert.ToInt32(Request.Form["hdid"]),
-                Description = Request.Form["tbDescription"],
-                Code = Request.Form["tbCode"],
-                TypeID = Convert.ToInt32(Request.Form["ddlType"]),
-                Amount = Convert.ToInt32(Request.Form["tbAmount"]),
-                Price = Convert.ToDouble(Request.Form["tbPrice"])
+                ProductID = Convert.ToInt32(Request.Form["Selected.ProductID"]),
+                Description = Request.Form["Selected.Description"],
+                Code = Request.Form["Selected.Code"],
+                TypeID = Convert.ToInt32(Request.Form["Selected.TypeID"]),
+                Amount = Convert.ToInt32(Request.Form["Selected.Amount"]),
+                Price = Convert.ToDouble(Request.Form["Selected.Price"])
             };
 
             if (product.ProductID > 0)
--- SOURCE/RMdemo/WebApplication1/Models/Product.cs
+++ SOURCE/RMdemo/WebApplication1/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using WebApplication1.Data;
@@ -8,12 +9,21 @@ namespace WebApplication1.Models
 {
     public class Product : IProduct
     {
+        [Required]
         public int ProductID { get; set; }
+        [Required]
+        [StringLength(250,MinimumLength = 1)]
         public string Description { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 1)]
         public string Code { get; set; }
         public int TypeID { get; set; }
         public string TypeCode { get; set; }
+        [Required]
+        [Range(0, Int32.MaxValue, ErrorMessage = "Amount can't be negative")]
         public int Amount { get; set; }
+        [Required]
+        [Range(0.0, Double.MaxValue, ErrorMessage = "It has to cost something")]
         public double Price { get; set; }
     }
 }
